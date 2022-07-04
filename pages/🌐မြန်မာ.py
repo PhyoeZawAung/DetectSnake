e88c4bd160fdffb2c6a61100388ff7a585d2e09e
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-07-04 12:19:41 +0630

    Update 🌐မြန်မာ.py

--- pages/🌐မြန်မာ.py
+++ pages/🌐မြန်မာ.py
@@ -10,6 +10,45 @@ def main():
         upload_photo()
     else:
         shoot_photo()
+    if st.button("ခွဲခြားမည"):
+        print("Classifing the snake photo in the model")
+        st.write("ခွဲခြားနေသည်........")
+        class_names = ['class-1','class-2','class-3', 'class-4', 'class-5']
+        Ans = prediction(photo,"snake_species.h5")
+        string=class_names[np.argmax(Ans)]
+        st.header(string)
+        st.write(Ans)
+        
+        
+
+
+
+def prediction(img, weights_file):#this is copy file of road sign project
+    # Load the model
+    model = keras.models.load_model(weights_file)
+   
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = img
+    # image sizing
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 255)
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+   # prediction_percentage = model.predict(data)
+    #prediction = prediction_percentage.round()
+    return prediction
+
+    #return prediction, prediction_percentage
 
 
 def shoot_photo():
