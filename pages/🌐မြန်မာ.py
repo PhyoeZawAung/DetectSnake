12dfc3dee7848911fb7a4f8b9045b8bac774d052
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-07-05 11:38:08 +0630

    Update 🌐မြန်မာ.py

--- pages/🌐မြန်မာ.py
+++ pages/🌐မြန်မာ.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-
+import streamllit.compontents.v1 as components
 import tensorflow as tf
 import cv2
 import pandas as pd
@@ -14,6 +14,8 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 from tensorflow.keras.models import load_model
+
+
 def main():
     st.set_page_config(
         page_title="မြွေအမျိုးအစားခွဲခြားခြင်း ",
@@ -32,23 +34,25 @@ def main():
                                          ('ပုံကိုတင်မည် ', 'ပုံရိုက်မည် '))
 
     if upload_option == 'ပုံကိုတင်မည် ':
-       photo = upload_photo()
+        photo = upload_photo()
     else:
         photo = shoot_photo()
     if st.button("ခွဲခြားမည် "):
         print("Classifing the snake photo in the model")
-        
-        class_names = ['Nerodia sipedon - Nothern Watersnake','Thamnophis sirtalis - Common Garter snake','Storeria dekayi -DeKay\'s Brown snake', 'Patherophis obsoletus - Black Rat snake', 'Cortalus atrox - Western Diamondback rattlesnake']
+
+        class_names = ['Nerodia sipedon - Nothern Watersnake', 'Thamnophis sirtalis - Common Garter snake',
+                       'Storeria dekayi -DeKay\'s Brown snake', 'Patherophis obsoletus - Black Rat snake',
+                       'Cortalus atrox - Western Diamondback rattlesnake']
         with st.spinner("ခွဲခြားနေသည်........"):
-           Ans = prediction(photo,"snake_species.h5")
-        string=class_names[np.argmax(Ans)]
+            Ans = prediction(photo, "snake_species.h5")
+        string = class_names[np.argmax(Ans)]
         st.header(string)
         st.write(Ans)
-     if st.sidebar.button("ဆက်သွယ်ရန် "):
-        contact()   
-        
-def contact():
+    if st.sidebar.button("ဆက်သွယ်ရန် "):
+        contact()
 
+
+def contact():
     form_submit = """<form action="https://formsubmit.co/phyoezawaung9696@gmail.com" method="POST">
      <input type="text" name="name" placeholder="အမည် "required>
      <input type="email" name="email" placeholder="အီးမေး">
@@ -87,14 +91,14 @@ button[type=submit]:hover
 
 </style>
      """
-    #st.markdown(form_submit,unsafe_allow_html=True)
-    components.html(form_submit,height =500)
+    # st.markdown(form_submit,unsafe_allow_html=True)
+    components.html(form_submit, height=500)
 
 
-def prediction(img, weights_file):#this is copy file of road sign project
+def prediction(img, weights_file):  # this is copy file of road sign project
     # Load the model
     model = keras.models.load_model(weights_file)
-   
+
     # Create the array of the right shape to feed into the keras model
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     image = img
@@ -112,11 +116,11 @@ def prediction(img, weights_file):#this is copy file of road sign project
 
     # run the inference
     prediction = model.predict(data)
-   # prediction_percentage = model.predict(data)
-    #prediction = prediction_percentage.round()
+    # prediction_percentage = model.predict(data)
+    # prediction = prediction_percentage.round()
     return prediction
 
-    #return prediction, prediction_percentage
+    # return prediction, prediction_percentage
 
 
 def shoot_photo():
