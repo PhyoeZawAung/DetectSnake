307397e1fda232435afb539b628dfabe4f7c3788
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-07-05 19:04:13 +0630

    Update SnakeDetectionMain.py

--- SnakeDetectionMain.py
+++ SnakeDetectionMain.py
@@ -1,20 +1,18 @@
-import streamlit as st
+from unicodedata import name
 import streamlit.components.v1 as components
-from streamlit_option_menu import option_menu
-
-import tensorflow as tf
 import cv2
-import pandas as pd
-from PIL import Image, ImageOps
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import PIL
-
+import seaborn as sns
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+from streamlit_option_menu import option_menu
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Sequential, load_model
 
 
 def main():
@@ -26,43 +24,68 @@ def main():
         menu_items={
 
             'About': """This app is made especially for farmer and can use for educational purpose. 
-            This app can classify the major 5 snake species.
-            We also develop for the rest of species. We never stop develop.""",
+                This app can classify the major 5 snake species.
+                We also develop for the rest of species. We never stop develop.""",
         }
     )
     st.title("Snake Species Classification")
-    st.header("this is header")
-    st.subheader('This is a subheader')
-    st.caption('This is a string that explains something above.')
     upload_option = st.sidebar.selectbox("How you want to make photo",
-                                         ('Upload', 'Shoot Photo'))
-
+                                         ('Upload','Shoot Photo'))
 
     if upload_option == 'Upload':
         photo = upload_photo()
     else:
         photo = shoot_photo()
     if st.button("Classifiy snake"):
-        if photo is not None:
-            print("Classifing the snake photo in the model")
+        print("Classifing the snake photo in the model")
 
-            class_names = ['Nerodia sipedon - Nothern Watersnake', 'Thamnophis sirtalis - Common Garter snake',
-                   'Storeria dekayi -DeKay\'s Brown snake', 'Patherophis obsoletus - Black Rat snake',
-                   'Cortalus atrox - Western Diamondback rattlesnake']
-            with st.spinner("Classifying snake specie"):
-                Ans = prediction(photo, "snake_species(1).h5")
-            if np.max(Ans) < 0.7 :
-                st.warning("Please shoot snake photo clearly")
-            else:
-                string = class_names[np.argmax(Ans)]
-                st.header(string)
-                st.write(Ans)
-        else:
-            st.warning("Please Upload or Shoot photo before classifying")
+
+        with st.spinner("Classifying snake specie"):
+            Ans = prediction(photo, "snake_species(1).h5")
+
+
+        res = Ans['name'].iat[0];
+        st.header(res)
+
+        links = {
+            'Nerodia sipedon - Northern Watersnake': 'https://en.wikipedia.org/wiki/Common_watersnake',
+            'Thamnophis sirtalis - Common Garter snake': 'https://en.wikipedia.org/wiki/Common_garter_snake',
+            "Storeria dekayi - DeKay's Brown snake": 'https://en.wikipedia.org/wiki/DeKay%27s_brown_snake',
+            'Patherophis obsoletus - Black Rat snake': 'https://en.wikipedia.org/wiki/Pantherophis_obsoletus',
+            'Crotalus atrox - Western Diamondback rattlesnake': 'https://en.wikipedia.org/wiki/Western_diamondback_rattlesnake',
+            'Others': '',
+        }
+
+        is_venomous = {
+            'Nerodia sipedon - Northern Watersnake': "<span style='color: green'>Non-venomous</span>",
+            'Thamnophis sirtalis - Common Garter snake': "<span style='color: darkorange'>Mildly Venomous</span>",
+            "Storeria dekayi - DeKay's Brown snake": "<span style='color: green'>Non-venomous</span>",
+            'Patherophis obsoletus - Black Rat snake': "<span style='color: green'>Non-venomous</span>",
+            'Crotalus atrox - Western Diamondback rattlesnake': "<span style='color: red'>Venomous</span>",
+            'Others': 'Sorry... The species in the image cannot be detected by our model.',
+        }
+    
+
+        st.markdown(is_venomous[res], unsafe_allow_html=True)
+        if res != 'Others':
+            st.write("For More Information. Check out this [link]("+links[res]+")")
+        st.markdown("<span style='color: darkorange'>Warning: A snake bite should always be taken to a hospital. Snake bites require hospitalization and close monitoring of the patient, whether the sanke is venomous or not.</span>", unsafe_allow_html=True)
+
+        st.markdown("""---""")
+        st.header("Confidence Chart")
+        fig, ax = plt.subplots()
+
+        ax  = sns.barplot(y = 'name',x='values', data = Ans,order = Ans.sort_values('values',ascending=False).name)
+
+        ax.set(xlabel='Confidence %', ylabel='Species')
+
+        for i in ax.containers:
+            ax.bar_label(i,)
+
+        st.pyplot(fig)
     if st.sidebar.button("Contect Developer"):
         contact()
-
-
+        
 def contact():
     form_submit = """<form action="https://formsubmit.co/phyoezawaung9696@gmail.com" method="POST">
      <input type="text" name="name" placeholder=" Name "required>
@@ -104,12 +127,23 @@ button[type=submit]:hover
      """
     # st.markdown(form_submit,unsafe_allow_html=True) this is not work css of button class
     components.html(form_submit, height=500)
+        
 
 
-def prediction(img, weights_file):  # this is copy file of road sign project
+
+def prediction(img, weights_file):#this is copy file of road sign project
+
+    class_names = [
+            "Nerodia sipedon - Northern Watersnake",
+            "Thamnophis sirtalis - Common Garter snake",
+            "Storeria dekayi - DeKay's Brown snake", 
+            "Patherophis obsoletus - Black Rat snake", 
+            "Crotalus atrox - Western Diamondback rattlesnake",
+            'Others'
+        ]
     # Load the model
     model = keras.models.load_model(weights_file)
-
+   
     # Create the array of the right shape to feed into the keras model
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     image = img
@@ -126,13 +160,29 @@ def prediction(img, weights_file):  # this is copy file of road sign project
     data[0] = normalized_image_array
 
     # run the inference
-    prediction = model.predict(data)
-    # prediction_percentage = model.predict(data)
-    # prediction = prediction_percentage.round()
+    prediction = model.predict(data)*100
+
+    prediction = pd.DataFrame(np.round(prediction,1),columns = class_names).transpose()
+
+    prediction.columns = ['values']
+
+    prediction  = prediction.nlargest(6, 'values')
+
+    prediction = prediction.reset_index()
+
+    prediction.columns = ['name', 'values']
+
+   # prediction_percentage = model.predict(data)
+    #prediction = prediction_percentage.round()
     return prediction
 
-    # return prediction, prediction_percentage
+    #return prediction, prediction_percentage
 
+
+
+
+
+     
 
 def shoot_photo():
     snake_image = st.camera_input("Shoot photo to calssify snake")
@@ -142,7 +192,6 @@ def shoot_photo():
         st.sidebar.image(img)
         return img
 
-
 def upload_photo():
     snake_image = st.file_uploader("Upload the file")
     if snake_image is not None:
@@ -151,5 +200,5 @@ def upload_photo():
         st.sidebar.image(img)
         return img
 
-
 main()
+
